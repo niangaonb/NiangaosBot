3647d28ba4ace1275214d55fff69815c8123b1eb
niangaoa <niangaoa@hotmail.com>
2023-05-01 16:41:54 +0800

    Add thread optimize

--- src/main/kotlin/com/niangaoa/niangaosbot/factory/BotMessageFactory.kt
+++ src/main/kotlin/com/niangaoa/niangaosbot/factory/BotMessageFactory.kt
@@ -10,7 +10,7 @@ class BotMessageFactory {
     fun getEvent(className: String) : Thread? {
         when (className) {
             "Communicate" -> return Thread(BotCommunicate())
-            "Random" -> return Thread(BotCommunicate())
+            "Random" -> return Thread(BotRandom())
             "Help" -> return Thread(BotHelp())
         }
         return null
